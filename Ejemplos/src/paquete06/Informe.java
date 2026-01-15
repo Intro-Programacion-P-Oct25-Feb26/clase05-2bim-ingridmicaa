c2d194663af1c375fce3ae9cf10d533065ca7a55
ingridmicaa <ingridmicaela03@gmail.com>
2026-01-15 11:16:39 -0500

    Paquete06

---
+++ Ejemplos/src/paquete06/Informe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete06;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class Informe {
+    public static void imprimir (int a , int b, int ab, int d) {
+        
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a, b,
+                ab,
+                a, b,
+                d);
+    }
+}
+
+
